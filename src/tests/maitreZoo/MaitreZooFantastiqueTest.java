75ea3cde63539bc65f1f6be76213fabce2ccaa6b
v22020869 <lyceeerwan.vincent@gmail.com>
2023-12-06 13:41:41 +0100

    Création de test pour le maitre de zoo

---
+++ src/tests/maitreZoo/MaitreZooFantastiqueTest.java
@@ -0,0 +1,53 @@
+package tests.maitreZoo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaitreZooFantastiqueTest {
+
+    @Test
+    void getAge() {
+
+    }
+
+    @Test
+    void setAge() {
+    }
+
+    @Test
+    void getNom() {
+    }
+
+    @Test
+    void setNom() {
+    }
+
+    @Test
+    void getSexe() {
+    }
+
+    @Test
+    void setSexe() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void examinerEnclos() {
+    }
+
+    @Test
+    void nettoyerEnclos() {
+    }
+
+    @Test
+    void nourrirCreatures() {
+    }
+
+    @Test
+    void deplacerCreature() {
+    }
+}
